195caf9308f7b4f20a21a15d8eccc497e4250afd
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-21 16:47:14 +0100

    my login system

---
+++ src/main/java/com/semicolon/africa/passwordManagementSystem/dtos/response/SearchUrlResponse.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa.passwordManagementSystem.dtos.response;
+
+
+import lombok.Data;
+
+@Data
+public class SearchUrlResponse {
+
+    private String username;
+    private String password;
+//    private String msg;
+
+}
