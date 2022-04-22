052aa09497e9ba3730f3e6e72a030b2c4962c02d
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-22 14:08:13 +0100

    my login system

---
+++ src/main/java/com/semicolon/africa/passwordManagementSystem/exception/CannotAddPasswordException.java
@@ -0,0 +1,8 @@
+package com.semicolon.africa.passwordManagementSystem.exception;
+
+public class CannotAddPasswordException extends RuntimeException{
+
+    public CannotAddPasswordException(String message){
+        super(message);
+    }
+}
