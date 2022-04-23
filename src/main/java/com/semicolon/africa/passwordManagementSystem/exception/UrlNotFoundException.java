89831c5f295c5cf2bf0bfa0d45030758e7124c1f
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-23 17:48:14 +0100

    my pasword app

---
+++ src/main/java/com/semicolon/africa/passwordManagementSystem/exception/UrlNotFoundException.java
@@ -0,0 +1,8 @@
+package com.semicolon.africa.passwordManagementSystem.exception;
+
+public class UrlNotFoundException extends RuntimeException {
+
+    public UrlNotFoundException(String message){
+        super(message);
+    }
+}
