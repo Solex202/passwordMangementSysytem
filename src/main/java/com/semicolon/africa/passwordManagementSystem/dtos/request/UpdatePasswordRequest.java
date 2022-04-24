e9393d7085776002b085e9ad9db9799223b658a4
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-24 11:01:37 +0100

    my password system

---
+++ src/main/java/com/semicolon/africa/passwordManagementSystem/dtos/request/UpdatePasswordRequest.java
@@ -0,0 +1,15 @@
+package com.semicolon.africa.passwordManagementSystem.dtos.request;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UpdatePasswordRequest {
+    private String url;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+}
