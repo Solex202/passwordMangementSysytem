4ecf9a941b84111f4855399d14607e03041338e9
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-19 13:43:00 +0100

    latest commit

---
+++ src/main/java/com/semicolon/africa/passwordManagementSystem/exception/UrlAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.semicolon.africa.passwordManagementSystem.exception;
+
+public class UrlAlreadyExistsException extends RuntimeException{
+
+    public UrlAlreadyExistsException(String message){
+        super(message);
+    }
+}
